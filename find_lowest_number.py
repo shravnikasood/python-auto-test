8c4d16689963478245eb38b7694883b4d7fe905d
shravnikasood <160657801+shravnikasood@users.noreply.github.com>
2024-04-07 00:06:15 -0400

    Update find_lowest_number.py

--- find_lowest_number.py
+++ find_lowest_number.py
@@ -1,18 +1,3 @@
-# The script will find the lowest number in a file and write it to another file.
-#
-# Run as: python3 find_lowest_number.py <input_file> <output_file>
-#
-# Example: python3 find_lowest_number.py numbers.txt lowest_number.txt
-#
-# If python is setup to run as "python" instead of "python3" on the machine, 
-# then we should use "python" instead of "python3" in the above.
-#
-# The input file should contain one number per line. The output file will 
-# contain the lowest number.
-#
-# If the input file is blank, the output file will contain the text: "No 
-# numbers found in file".
-
 import sys
 
 input_filename = sys.argv[1]
